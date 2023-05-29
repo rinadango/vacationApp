8f76f1e8bb12c7257958a9ff9307d80b9260d1e6
rinadango <ipaulachan@gmail.com>
2023-05-29 23:59:45 +0300

    Created a form controller

--- src/main/java/com/vacationapp/controller/DestinationController.java
+++ src/main/java/com/vacationapp/controller/DestinationController.java
@@ -34,28 +34,6 @@ public class DestinationController {
         return "cities";
     }
 
-    @GetMapping("/proposal")
-    public String showProposalPage(){
-
-        return "proposal";
-    }
-
-    @PostMapping("/proposal")
-    public String submitForm(@RequestParam("city") String city,
-                             @RequestParam("budget") String budget,
-                             @RequestParam("startOfVacation") String startOfVacation,
-                             @RequestParam("endOfVacation") String endOfVacation,
-                             Model model) {
-        // Process the form data (e.g., save it to a database)
-
-        // Pass the form data to the view
-        model.addAttribute("city", city);
-        model.addAttribute("budget", budget);
-        model.addAttribute("startVaca", startOfVacation);
-        model.addAttribute("endVaca", endOfVacation);
-
-        return "proposal";
-    }
 
 }
 
