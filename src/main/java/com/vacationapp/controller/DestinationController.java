18cf4abcf02c03e5352a4bff4e406be19c259bf3
karolinapauz <karolina.pauziene@gmail.com>
2023-05-30 19:38:42 +0300

    from yesterday

--- src/main/java/com/vacationapp/controller/DestinationController.java
+++ src/main/java/com/vacationapp/controller/DestinationController.java
@@ -5,6 +5,7 @@ import com.vacationapp.service.DestinationService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
